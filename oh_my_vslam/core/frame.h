ab3ad4ad89ca6ba13e18d7d1a7bc3ddd7c8d999d
feixyz10 <fei-china@139.com>
2021-04-22 17:57:40 +0800

    frame and feature

--- oh_my_vslam/core/frame.h
+++ oh_my_vslam/core/frame.h
@@ -3,12 +3,13 @@
 #include <memory>
 #include <opencv2/core/core.hpp>
 
+#include "common/common.h"
 #include "common/geometry/pose3d.h"
 #include "oh_my_vslam/core/camera.h"
 
 namespace oh_my_vslam {
 
-enum class FrameType { UNKNOWN = 0, MONO, STEREO, RGBD };
+struct Feature;
 
 class Frame {
  public:
@@ -16,19 +17,17 @@ class Frame {
   using ConstPtr = std::shared_ptr<const Frame>;
 
  public:
-  Frame() = default;
-
-  Frame(const size_t id, const double timestamp, const cv::Mat &img = {},
-        const Camera::ConstPtr &camera = nullptr,
-        const common::Pose3d &pose_c2w = {})
-      : id_(id),
-        timestamp_(timestamp),
+  Frame(const double timestamp, const cv::Mat &img,
+        const Camera::ConstPtr &camera, const common::Pose3d &pose_c2w = {})
+      : timestamp_(timestamp),
         img_(img),
         camera_(camera),
         pose_c2w_(pose_c2w),
-        pose_w2c_(pose_c2w.Inv()) {}
-
-  virtual ~Frame() = default;
+        pose_w2c_(pose_c2w.Inv()) {
+    ACHECK(camera != nullptr);
+    static size_t static_id = 0;
+    id_ = static_id++;
+  }
 
   size_t id() const {
     return id_;
@@ -38,7 +37,7 @@ class Frame {
     return timestamp_;
   }
 
-  cv::Mat img() const {
+  const cv::Mat &img() const {
     return img_;
   }
 
@@ -55,38 +54,34 @@ class Frame {
     pose_w2c_ = pose_c2w.Inv();
   }
 
-  // check if a point in world coordinate is seen by the frame
-  virtual bool IsInFrame(const Eigen::Vector3d &pt_w) const;
+  void SetKeyFrame() {
+    static size_t static_keyframe_id = 0;
+    is_keyframe_ = true;
+    keyframe_id_ = static_keyframe_id++;
+  }
 
-  virtual FrameType frame_type() const {
-    return FrameType::UNKNOWN;
+  const std::vector<std::shared_ptr<Feature>> &features() const {
+    return features_;
+  }
+
+  std::vector<std::shared_ptr<Feature>> &features() {
+    return features_;
   }
 
  protected:
   size_t id_ = 0;
   double timestamp_ = -1.0;
+  bool is_keyframe_ = false;
+  size_t keyframe_id_ = 0;
   cv::Mat img_;
   Camera::ConstPtr camera_{nullptr};
   common::Pose3d pose_c2w_{}, pose_w2c_{};
+  std::vector<std::shared_ptr<Feature>> features_;
+
+  DISALLOW_COPY_AND_ASSIGN(Frame);
 };
 
-class MonoFrame : public Frame {
- public:
-  using Ptr = std::shared_ptr<MonoFrame>;
-  using ConstPtr = std::shared_ptr<const MonoFrame>;
-
- public:
-  MonoFrame() = default;
-
-  MonoFrame(const size_t id, const double timestamp, const cv::Mat &img = {},
-            const Camera::ConstPtr &camera = nullptr,
-            const common::Pose3d &pose_c2w = {})
-      : Frame(id, timestamp, img, camera, pose_c2w){};
-
-  FrameType frame_type() const override {
-    return FrameType::MONO;
-  }
-};
+using MonoFrame = Frame;
 
 class RGBDFrame : public Frame {
  public:
@@ -94,19 +89,18 @@ class RGBDFrame : public Frame {
   using ConstPtr = std::shared_ptr<const RGBDFrame>;
 
  public:
-  RGBDFrame() = default;
-
-  RGBDFrame(const size_t id, const double timestamp, const cv::Mat &img = {},
-            const cv::Mat &depth = {}, const Camera::ConstPtr &camera = nullptr,
+  RGBDFrame(const double timestamp, const cv::Mat &img, const cv::Mat &depth,
+            const Camera::ConstPtr &camera = nullptr,
             const common::Pose3d &pose_c2w = {})
-      : Frame(id, timestamp, img, camera, pose_c2w), depth_(depth) {}
+      : Frame(timestamp, img, camera, pose_c2w), depth_(depth) {}
 
-  FrameType frame_type() const override {
-    return FrameType::RGBD;
+  const cv::Mat &depth() const {
+    return depth_;
   }
 
  protected:
   cv::Mat depth_;
+  DISALLOW_COPY_AND_ASSIGN(RGBDFrame);
 };
 
 class StereoFrame : public Frame {
@@ -115,29 +109,39 @@ class StereoFrame : public Frame {
   using ConstPtr = std::shared_ptr<const StereoFrame>;
 
  public:
-  StereoFrame() = default;
+  StereoFrame(const double timestamp, const cv::Mat &img_lft,
+              const cv::Mat &img_rgt, const Camera::ConstPtr &camera = nullptr,
+              const double baseline = 0.0, const common::Pose3d &pose_c2w = {})
+      : Frame(timestamp, img_lft, camera, pose_c2w),
+        img_rgt_(img_rgt),
+        baseline_(baseline) {}
 
-  StereoFrame(const size_t id, const double timestamp, const cv::Mat &img = {},
-              const cv::Mat &img2 = {},
-              const Camera::ConstPtr &camera = nullptr,
-              const Camera::ConstPtr &camera2 = nullptr,
-              const common::Pose3d &pose_c2w = {},
-              const common::Pose3d &pose_rel = {})
-      : Frame(id, timestamp, img, camera, pose_c2w),
-        img2_(img2),
-        camera2_(camera2),
-        pose_rel_(pose_rel) {}
-
-  FrameType frame_type() const override {
-    return FrameType::STEREO;
+  const cv::Mat &img_rgt() const {
+    return img_rgt_;
   }
 
-  bool IsInFrame(const Eigen::Vector3d &pt_w) const override;
+  double baseline() const {
+    return baseline_;
+  }
+
+  const std::vector<std::shared_ptr<Feature>> &features_rgt() const {
+    return features_rgt_;
+  }
+
+  std::vector<std::shared_ptr<Feature>> &features_rgt() {
+    return features_rgt_;
+  }
+
+  double Disparity(const Eigen::Vector3d &pt_w) {
+    Eigen::Vector3d pt_c = pose_w2c_ * pt_w;
+    return baseline_ * camera_->intrinsic_params()(0) / pt_c.z();
+  }
 
  protected:
-  cv::Mat img2_;                       // right image
-  Camera::ConstPtr camera2_{nullptr};  // right camera
-  common::Pose3d pose_rel_{};  // relative pose of right camera to left camera
+  cv::Mat img_rgt_;       // right image
+  double baseline_{0.0};  // baseline
+  std::vector<std::shared_ptr<Feature>> features_rgt_;
+  DISALLOW_COPY_AND_ASSIGN(StereoFrame);
 };
 
 }  // namespace oh_my_vslam
